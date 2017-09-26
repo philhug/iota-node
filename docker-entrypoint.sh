f73ecd83404384a51c9ee11d718a9e81434ba183
Philipp Hug <philipp@hug.cx>
2017-09-26 18:42:42 +0200

    updated to 1.4

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,5 @@ for buddy in $(cat /iri/conf/neighbors); do
 done
 neighbors=${neighbors::-1}
 
-exec java -XX:+DisableAttachMechanism -Xmx8g -Xms256m -Dlogback.configurationFile=/iri/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri-1.3.2.2.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "addNeighbors, removeNeighbors" --send-limit 100.0 "$@"
+exec java -XX:+DisableAttachMechanism -Xmx8g -Xms256m -Dlogback.configurationFile=/iri/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri-1.4.0.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "addNeighbors, removeNeighbors" --send-limit 100.0 "$@"
 
